f67c6644c75b9de2ad53019a6cc39bfc186411d0
jeauk <qufskswodnr@gmail.com>
2024-08-10 13:32:10 +0900

    first project

---
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/SpringbootRecycleApplication.java
@@ -0,0 +1,13 @@
+package com.trashformer.springboot_recycle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRecycleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRecycleApplication.class, args);
+	}
+
+}
---
+++ springboot-recycle/src/test/java/com/trashformer/springboot_recycle/SpringbootRecycleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trashformer.springboot_recycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRecycleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
