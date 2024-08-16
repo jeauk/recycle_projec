9e7b5941770a9000dcee4228c2624c504bf7767d
Huigon <gmlrhs281@naver.com>
2024-08-16 12:43:28 +0900

    아름다운가게 / 굿윌스토어 컨트롤러, 덴티티, 레파지토리 추가

---
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/controller/GwController.java
@@ -0,0 +1,25 @@
+package com.trashformer.springboot_recycle.controller;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.trashformer.springboot_recycle.entity.GwEntity;
+import com.trashformer.springboot_recycle.repository.GwRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class GwController {
+  @Autowired GwRepository gwRepository;
+
+  @GetMapping("/gwill")
+  public List<GwEntity> getGwill() {
+    List<GwEntity> list = gwRepository.findAll();
+      return list;
+  }
+  
+}
---
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/entity/GwEntity.java
@@ -0,0 +1,18 @@
+package com.trashformer.springboot_recycle.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class GwEntity {
+  @Id
+  Long id;
+
+  String name;
+  String address;
+  String tel;
+  String lat;
+  String lng;
+}
