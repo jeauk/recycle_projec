4039dd06cfd677ce76478d1ae336fdb9fd412aa1
Huigon <gmlrhs281@naver.com>
2024-08-16 12:59:46 +0900

    엔티티 각각 데이터베이스 자동 증가 추가

--- springboot-recycle/src/main/java/com/trashformer/springboot_recycle/entity/GwEntity.java
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/entity/GwEntity.java
@@ -1,12 +1,15 @@
 package com.trashformer.springboot_recycle.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
 @Entity
 public class GwEntity {
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Id
   Long id;
 
