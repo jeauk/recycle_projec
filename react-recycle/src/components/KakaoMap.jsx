1154442d92973cd1bb997e144ff91037ad0ebebd
korea <wakt7568@naver.com>
2024-08-19 17:41:42 +0900

    리액트 페이지 병합 및 지도에 자판기 위치 표시

--- react-recycle/src/components/KakaoMap.jsx
+++ react-recycle/src/components/KakaoMap.jsx
@@ -1,12 +1,9 @@
 import { Map } from "react-kakao-maps-sdk";
+import KakaoMapMarker from "./KakaoMapMarker";
 
 const KakaoMap = () => {
     return (
-        <Map
-            center={{ lat: 33.5563, lng: 126.79581 }}   // 지도의 중심 좌표
-            style={{ width: '800px', height: '600px' }} // 지도 크기
-            level={3}                                   // 지도 확대 레벨
-        />
+                <KakaoMapMarker />
     );
 };
 
