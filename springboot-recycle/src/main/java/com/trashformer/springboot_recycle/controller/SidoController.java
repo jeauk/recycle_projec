68df57d585ebd5fc15cd04759300de4ea8b1805a
Huigon <gmlrhs281@naver.com>
2024-08-21 15:15:38 +0900

    데이터 확인용 컨트롤러 제거 및 임포트 정리

--- springboot-recycle/src/main/java/com/trashformer/springboot_recycle/controller/SidoController.java
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/controller/SidoController.java
@@ -4,8 +4,6 @@ package com.trashformer.springboot_recycle.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.trashformer.springboot_recycle.entity.SidoEntity;
@@ -23,16 +21,3 @@ public class SidoController {
       return list;
   }
 }
-
-// @Controller
-// public class SidoController {
-//   @Autowired SidoRepository sidoRepository;
-
-//   @GetMapping("/sido")
-//   public String sido(Model model) {
-//     List<SidoEntity> list = sidoRepository.findAll();
-//     model.addAttribute("list", list);
-//     return "sido";
-//   }
-// }
-
