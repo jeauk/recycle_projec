27f3df05609557ab3f6bc458d3c97e53d1a88ace
jeauk <qufskswodnr@gmail.com>
2024-08-21 16:33:10 +0900

    KakaoUserEntity ID 값 들어가게 바꾼거

--- springboot-recycle/src/main/java/com/trashformer/springboot_recycle/entity/KakaoUserEntity.java
+++ springboot-recycle/src/main/java/com/trashformer/springboot_recycle/entity/KakaoUserEntity.java
@@ -1,9 +1,12 @@
 package com.trashformer.springboot_recycle.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 
 @Entity(name = "kakaoUser")
@@ -17,5 +20,7 @@ public class KakaoUserEntity {
     private String nickname;
     private String email;
     private String profileImageUrl;
-    
+ 
+    @OneToMany(mappedBy = "kakaoUserEntity")
+    private List<ReformBoardEntity> boards;
 }
