5c6268b1c44b952651eaff8cd05faee208abdf84
LSG <tkdruddl20@naver.com>
2024-08-21 13:29:19 +0900

    나브 모듈 만드는 중

--- react-recycle/src/components/Nav.jsx
+++ react-recycle/src/components/Nav.jsx
@@ -1,11 +1,14 @@
 import { Link } from "react-router-dom";
+import styles from '../styles/Nav.module.css';
 
 const Nav = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container px-lg-5">
                 <Link className="navbar-brand" to="#!">Start Bootstrap</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon" />
+                </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item"><Link className="nav-link active" aria-current="page" to="#!">Home</Link></li>
