20dd51769f70cff6de545e0bb0fceed3f9a073b1
LSG <tkdruddl20@naver.com>
2024-08-28 16:25:46 +0900

    topcontents 완성

--- react-recycle/src/MainPage/TopContents.jsx
+++ react-recycle/src/MainPage/TopContents.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import st from '../styles/TopContents.module.css';
 
 const TopContents = () => {
@@ -8,6 +9,7 @@ const TopContents = () => {
                     <div className={st.c4}>
                         <h1 className={st.h1}>hell</h1>
                         <p className={st.p1}>hello</p>
+                        <Link className={st.lk} to="#">sdsd</Link>
                     </div>
                 </div>
             </div>
