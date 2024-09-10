c847e0bb3dd9bea4a59153f15a13f73262fe65e3
LSG <tkdruddl20@naver.com>
2024-09-10 14:23:00 +0900

    contact develop에서 가져옴

--- react-recycle/src/components/Contact.jsx
+++ react-recycle/src/components/Contact.jsx
@@ -1,4 +1,6 @@
 import { useRef, useState } from "react";
+import style from '../styles/Contact.module.css';
+import { getFileHash } from "../utils";
 
 const Contact = () => {
     const [name, setName] = useState('');
@@ -7,19 +9,50 @@ const Contact = () => {
     const [text, setText] = useState('');
     const [images, setImages] = useState([]);
     const [imagePreviews, setImagePreviews] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const fileInputRef = useRef(null); // 파일 입력필드에 대한 참조
+    const [message, setMessage] = useState({ text: '', type: '', visible: false }); // type: 'success' or 'error'
 
-    const handleFileChange = (e) => {
-        const files = Array.from(e.target.files);
-        setImages(files);
+
+    const handleFileChange = async (e) => {
+        const newFiles = Array.from(e.target.files);
+        const newImages = [...images];
+        const newPreviews = [];
         
-        // 파일 미리보기 URL 생성
-        const previews = files.map(file => URL.createObjectURL(file));
-        setImagePreviews(previews);
+        for (const file of newFiles) {
+            const fileHash = await getFileHash(file);
+            // 중복 파일 체크
+            const isDuplicate = images.some(img => img.hash === fileHash);
+            
+            if (!isDuplicate) {
+                newImages.push({ file, hash: fileHash });
+                newPreviews.push(URL.createObjectURL(file));
+            }
+        }
+        
+        setImages(newImages);
+        setImagePreviews(prevPreviews => [...prevPreviews, ...newPreviews]);
     };
     
+    const handleImageDelete = (index) => {
+        // 이미지 배열에서 특정 이미지를 제거
+        setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+        // 이미지 미리보기 배열에서 특정 미리보기를 제거
+        setImagePreviews((prevPreviews) => prevPreviews.filter((_, i) => i !== index));
+    
+        // 파일 입력 필드를 초기화
+        if (fileInputRef.current) {
+            fileInputRef.current.value = '';
+        }
+    };
+
+    const closeMessage = () => {
+        setMessage({ text: '', type: '', visible: false });
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault(); // 기본 폼 제출 동작 방지
+        setIsLoading(true); // 로딩 시작
 
         const formData = new FormData();
         formData.append('name', name);
@@ -27,8 +60,8 @@ const Contact = () => {
         formData.append('subject', subject);
         formData.append('text', text);
         // 여러 파일을 추가
-        images.forEach((file) => {
-            formData.append('images', file);
+        images.forEach((img) => {
+            formData.append('images', img.file);
         });
 
         const url = "http://localhost:8080/api/contact/post";
@@ -39,7 +72,7 @@ const Contact = () => {
             });
 
             if (response.ok) {
-                alert("문의가 접수되었습니다"); // 메시지 창 표시
+                setMessage({ text: "문의가 접수되었습니다.", type: 'success', visible: true});
                 // 폼 초기화
                 setName('');
                 setReplyTo('');
@@ -57,12 +90,28 @@ const Contact = () => {
             }
         } catch (error) {
             console.error('Error:', error);
-            alert(error.message);
+            setMessage({ text: error.message, type: 'error', visible: true});
+        } finally {
+            setIsLoading(false); // 로딩 종료
         }
     };
 
     return (
         <form onSubmit={handleSubmit}>
+            {isLoading && (
+                <>
+                    <div className={style.spinner} />
+                    <div className={style.spinnerOverlay} />
+                </>
+            )}
+            {message.text && (
+                <div className={`${style.message} ${style[`${message.type}Message`]}`}>
+                    {message.text}
+                    <button className={style.messageButton} type="button" onClick={closeMessage}>
+                    확인
+                </button>
+                </div>
+            )}
             <div>
                 <div>이름(닉네임)</div>
                 <input
@@ -70,6 +119,7 @@ const Contact = () => {
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                     required
+                    disabled={isLoading}
                 />
             </div>
             <div>
@@ -79,6 +129,7 @@ const Contact = () => {
                     value={replyTo}
                     onChange={(e) => setReplyTo(e.target.value)}
                     required
+                    disabled={isLoading}
                 />
             </div>
             <div>
@@ -88,6 +139,7 @@ const Contact = () => {
                     value={subject}
                     onChange={(e) => setSubject(e.target.value)}
                     required
+                    disabled={isLoading}
                 />
             </div>
             <div>
@@ -97,28 +149,42 @@ const Contact = () => {
                     value={text}
                     onChange={(e) => setText(e.target.value)}
                     required
+                    disabled={isLoading}
                 />
             </div>
             <div>
                 <div>이미지 첨부</div>
+                <div>1장씩 첨부시 첨부 순서대로 정렬되고 한번에 첨부시 파일 이름 순으로 정렬됩니다.</div>
                 <input
                     type="file"
                     accept="image/*"
                     multiple
                     ref={fileInputRef}
                     onChange={handleFileChange}
+                    disabled={isLoading}
                 />
                 <div>
                     {imagePreviews.map((preview, index) => (
-                        <img
-                            key={index}
-                            src={preview}
-                            alt={`preview-${index}`}
-                        />
+                        <div key={index} className={style.contactpreview}>
+                            <img
+                                className={style.contactpreviewimg}
+                                src={preview}
+                                alt={`preview-${index}`}
+                            />
+                            <button
+                                className={style.contactdeletebtn}
+                                type="button"
+                                onClick={() => handleImageDelete(index)}
+                                disabled={isLoading}
+                            >
+                                X
+                            </button>
+                        </div>
                     ))}
                 </div>
             </div>
             <button type="submit">보내기</button>
+            {isLoading && <div>메일을 보내는 중입니다...</div>}
         </form>
     );
 };
