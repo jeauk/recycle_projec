2d0c02ce12eb6303205c871cc2bfb6c910aca8bc
Huigon <gmlrhs281@naver.com>
2024-09-19 17:47:58 +0900

    자판기 배경 + 슈퍼빈 로고 추가

--- react-recycle/src/vendingDevice/KakaoMap.jsx
+++ react-recycle/src/vendingDevice/KakaoMap.jsx
@@ -8,6 +8,9 @@ const KakaoMap = () => {
         <div>
             <button className={map.btn1} onClick={() => { navigate('/sumap') }}>재활용 자판기</button>
             <button className={map.btn2} onClick={() => { navigate('/remap') }}>중고가게 위치</button>
+            <div className={map.bgc}>
+                <div className={map.super}></div>
+            </div>
             <div className={map.bg}></div>
         </div>
     );
