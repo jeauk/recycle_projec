67a25c0e006162a7d1638208d80c0cfbbc79d24c
l0cyp <l0cyp1127@gmail.com>
2024-09-24 09:10:33 +0900

    포스트폼 크롭수정

--- react-recycle/src/reformBoard/PostDetail.jsx
+++ react-recycle/src/reformBoard/PostDetail.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import styles from './PostDetail.module.css';
 
 function PostDetail() {
   const { id } = useParams(); // URL에서 게시물 ID를 가져옴
