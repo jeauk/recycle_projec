9b3bd7916c5eefc94f086df061d832b76a7b9cf2
Huigon <gmlrhs281@naver.com>
2024-09-25 09:48:27 +0900

    캐시된거 삭제하기

--- react-recycle/src/utils.js
+++
@@ -1,9 +0,0 @@
-export const getFileHash = async (file) => {
-    const arrayBuffer = await file.arrayBuffer();
-    const hashBuffer = await crypto.subtle.digest('SHA-256', arrayBuffer);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-};
-
-// 컨택트에 올라가는 파일을 해시하여 중복 파일을 제거하기위해 해시를 생성하는 유틸리티 함수
