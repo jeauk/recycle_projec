47c2e09ac8189e1062ee4c71495a4d2a5e011028
LSG <tkdruddl20@naver.com>
2024-09-26 13:47:50 +0900

    푸터 최종 변경

--- react-recycle/src/components/Footer.jsx
+++ react-recycle/src/components/Footer.jsx
@@ -1,11 +1,27 @@
-import s from '../styles/Footer.module.css'
-
+import { Link } from 'react-router-dom';
+import s from '../styles/Footer.module.css';
 
 const Footer = () => {
     return (
         <footer className={s.footer}>
-            <div className={s.footerContainer}>
-                <p className={s.containerText}>
+            <div className={s.footerMenu}>
+                <Link className={s.FMButton} to="/map"><div className={s.footername}>중고/재활용</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/sido"><div className={s.footername}>폐기물 전화번호</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/list"><div className={s.footername}>리폼 게시판</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/carvon/drafting"><div className={s.footername}>탄소중립포인트란?</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/carvon/incentive"><div className={s.footername}>탄소중립포인트 혜택</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/carvon/carvonMethod"><div className={s.footername}>탄소중립포인트 참여방법</div></Link>
+                <div className={s.FMButton}>|</div>
+                <Link className={s.FMButton} to="/carvon/companyList"><div className={s.footername}>탄소중립포인트 참여기업</div></Link>
+            </div>
+
+            <div className={s.copyrightContainer}>
+                <p className={s.copyrightText}>
                     Copyright &copy; TrashFormer 2024
                 </p>
             </div>
