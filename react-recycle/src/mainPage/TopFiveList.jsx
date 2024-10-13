5945f00e3bf5cdf1b51bf811562ab1c4f1e7244e
LSG <tkdruddl20@gmail.com>
2024-10-13 14:43:21 +0900

    인기글 5개 껍데기 수정

--- react-recycle/src/mainPage/TopFiveList.jsx
+++ react-recycle/src/mainPage/TopFiveList.jsx
@@ -1,12 +1,7 @@
 import { useEffect, useState } from "react";
-import Slider from "react-slick";
 import styles from "../styles/TopFiveList.module.css";  // CSS 모듈을 import
-import "../styles/TopFiveList.css";  // CSS 모듈을 import
 
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { useNavigate } from "react-router-dom";
-import { Margin } from "@mui/icons-material";
 
 const TopFiveList = () => {
     const myBackDomain = "https://trashformer.site";
@@ -42,22 +37,29 @@ const TopFiveList = () => {
     const clickHandler = ()=>{
         navigate('/list')
     }
-    // react-slick 슬라이더 설정
-    const settings = {
-        dots: true,  // 하단에 점(dot) 네비게이션 표시
-        infinite: true,  // 슬라이더가 무한 반복되도록 설정
-        speed: 500,  // 슬라이더 전환 속도
-        slidesToShow: 2.5,  // 한 번에 2.5개의 슬라이드를 보여줌
-        slidesToScroll: 1,  // 한 번에 넘어가는 슬라이드 수
-        autoplay: true,  // 자동 재생
-        autoplaySpeed: 6000,  // 6초마다 자동 슬라이드
-        arrows: true,  // 좌우 화살표를 통한 슬라이드 제어
-    };
 
     return (
         <div style={{display: 'block'}}>
-            {/* 제목과 더보기를 한 줄에 맞추고, 아랫부분을 기준으로 정렬 */}
             <div className={styles.titleContainer}>
+                <h2 className={styles.title}>인기 리폼 게시글</h2>
+                <p className={styles.more} onClick={clickHandler}>더 보기</p>
+            </div>
+            <div className={styles.viewContainer}>
+                <div className={styles.imgContainer}>
+                    <img src="/img/aa.jpeg" alt="test" />
+                </div>
+                <div className={styles.textContainer}>
+                    <div className={styles.TFTitle}>제목</div>
+                    <div className={styles.TFContents}>내용</div>
+                </div>
+            </div>
+            
+            
+            
+            
+            
+            
+            {/* <div className={styles.titleContainer}>
                 <h2 className={styles.title}>인기 리폼게시글</h2>
                 <p className={styles.more} onClick={clickHandler}>더보기</p>
             </div>
@@ -73,7 +75,7 @@ const TopFiveList = () => {
                         />
                     </div>
                 ))}
-            </Slider>
+            </Slider> */}
         </div>
     );
 };
