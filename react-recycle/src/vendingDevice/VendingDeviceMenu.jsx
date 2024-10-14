fef0196bb93a661262b8ed2f948dcec01ffebde2
korea <wakt7568@naver.com>
2024-10-14 10:06:05 +0900

    modal 설명 추가

--- react-recycle/src/vendingDevice/VendingDeviceMenu.jsx
+++ react-recycle/src/vendingDevice/VendingDeviceMenu.jsx
@@ -1,9 +1,16 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import Modal from 'react-modal';
 import m from '../styles/VendingDeviceMenu.module.css';
 
+Modal.setAppElement('#root');
 const VendingDeviceMenu = React.memo(({ locations, onLocationClick, searchHistory, setSearchHistory, loading }) => {
   const [isOpen, setIsOpen] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
+  const [activeButton, setActiveButton] = useState("nephron");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  const [dontShowToday, setDontShowToday] = useState(false);
+  const POPUP_KEY = `dontShowPopup_${activeButton}`;
   const toggleMenu = useCallback(() => {
     setIsOpen(!isOpen);
   }, [isOpen]);
@@ -38,10 +45,33 @@ const VendingDeviceMenu = React.memo(({ locations, onLocationClick, searchHistor
     setSearchHistory([]);
   }, [setSearchHistory]);
 
+  const handleDontShowTodayChange = useCallback(() => {
+    const expirationTime = new Date().getTime() + 24 * 60 * 60 * 1000;
+    setDontShowToday(!dontShowToday);
+    localStorage.setItem(POPUP_KEY, expirationTime);
+  }, [dontShowToday, POPUP_KEY]);
+
   const handleLocationClick = useCallback((loc) => {
     onLocationClick(loc);
   }, [onLocationClick])
+  const handleTabClick = useCallback((tab) => {
+    setActiveButton(tab);
+    setDontShowToday(false);
 
+    if (tab === "nephron") {
+      setModalContent({
+        title: "네프론 설명",
+        content: (
+          <>
+            네프론은 슈퍼빈의 재활용 자판기 시스템으로, 사용자가 지정된 재활용품을 깨끗하게 처리하여 투입하면 포인트로 보상받을 수 있는 서비스입니다. <br/><br/>
+            이 포인트는 1포인트 당 1원의 가치로 환전되며, 수퍼빈 홈페이지나 모바일 앱의 적립 내역 메뉴에서 확인할 수 있습니다. <br/><br/>
+            더 자세한 사항은 수퍼빈 질문 게시판에서 확인하실 수 있습니다.
+          </>
+        ),
+      });
+    }
+    setIsModalOpen(true);
+  }, []);
   useEffect(() => { // 표시되는 리스트가 바뀌면 스크롤을 초기화 하는 함수 (09-23-09:27)
     if (listRef.current) {
       listRef.current.scrollTop = 0;
@@ -86,10 +116,29 @@ const VendingDeviceMenu = React.memo(({ locations, onLocationClick, searchHistor
               ))
           )}
         </div>
+        <button className={m.descriptionButton} onClick={() => handleTabClick("nephron")}>네프론 설명</button>
       </div>
       <button className={`${m.toggleButton} ${isOpen ? m.open : ''}`} onClick={toggleMenu}>
         {isOpen ? '<' : '>'}
       </button>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        contentLabel="Popup Modal"
+        className={m.modal}
+        overlayClassName={m.overlay}
+      >
+        <h2>{modalContent?.title}</h2>
+        <p>{modalContent?.content}</p>
+        <label className={m.nottd}>
+          <input
+            type="checkbox"
+            checked={dontShowToday}
+            onChange={handleDontShowTodayChange}
+          /> 오늘 하루 보지 않기
+        </label>
+        <button onClick={() => setIsModalOpen(false)}>Close</button>
+      </Modal>
       {/* {isOpen && (
         <div className={`${m.logoPane} ${isOpen ? m.open : ''}`}>
           <img src="/img/logo.png" alt="Logo" style={{ width: '225px', height: 'auto' }} />
